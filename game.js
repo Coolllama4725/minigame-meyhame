eba0c7930f200ad913817371a1d79b9d4fe110f0
Coolllama4725 <cschmidt4725@gmail.com>
2026-09-04 06:29:18 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 // supabase connection
 
-const SUPABASE_URL = "https://pbvibpxgandnratdcxep.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://pbvibpxgandnratdcxep.supabase.co";
 const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_MIuMoqdA0X-1VGUMAvIROg_B6vIvI8t";
 
 const supabaseClient = window.supabase.createClient(
