db20a993111b03699a2b9e183928c135a18269f4
Coolllama4725 <cschmidt4725@gmail.com>
2026-09-04 05:42:42 -0400

    Update game.js

--- game.js
+++ game.js
@@ -2,3 +2,6 @@
 
 const SUPABASE_URL = "https://pbvibpxgandnratdcxep.supabase.co/rest/v1/";
 const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_MIuMoqdA0X-1VGUMAvIROg_B6vIvI8t";
+
+console.log("Minigame Mayhem connected to Supabase!");
+console.log("Supabase URL:", SUPABASE_URL);
