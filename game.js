8b251d14caba227450b8b5a32f1915dbaadc1163
Coolllama4725 <cschmidt4725@gmail.com>
2026-09-04 06:22:32 -0400

    Update game.js

--- game.js
+++ game.js
@@ -3,4 +3,9 @@
 const SUPABASE_URL = "https://pbvibpxgandnratdcxep.supabase.co/rest/v1/";
 const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_MIuMoqdA0X-1VGUMAvIROg_B6vIvI8t";
 
-alert("Minigame Mayhem's game code is working!");
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_PUBLISHABLE_KEY
+);
+
+console.log("Supabase client created!");
